ae96fc084e52afe1c0fee267a3a45c29ced03bf3
timothy-spencer <timothy.spencer@gsa.gov>
2020-07-31 11:41:18 -0700

    update repo location, add dashboard

--- base/dashboard/render-dashboard.sh
+++ base/dashboard/render-dashboard.sh
@@ -2,5 +2,11 @@
 #
 # get the dashboard!
 #
-curl https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml > dashboard.yaml
+#curl https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml > dashboard.yaml
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+helm repo update
+
+kubectl config set-context --current --namespace=kubernetes-dashboard
+helm template dashboard kubernetes-dashboard/kubernetes-dashboard > dashboard.yaml
 
