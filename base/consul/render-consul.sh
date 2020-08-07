6fb9f8ade9790c2304c1f1c367bb20257d1973dd
timothy-spencer <timothy.spencer@gsa.gov>
2020-08-07 09:59:22 -0700

    turn on injector

--- base/consul/render-consul.sh
+++ base/consul/render-consul.sh
@@ -5,5 +5,5 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
 
 kubectl config set-context --current --namespace=consul
-helm template consul hashicorp/consul --set global.name=consul > consul.yaml
+helm template consul hashicorp/consul -f consul-values.yaml > consul.yaml
 
