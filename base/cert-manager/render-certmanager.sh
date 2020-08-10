43287f595d275ace265c1401fec10f92a542c60b
timothy-spencer <timothy.spencer@gsa.gov>
2020-08-10 14:50:19 -0700

    namespace is causing problems maybe?

--- base/cert-manager/render-certmanager.sh
+++ base/cert-manager/render-certmanager.sh
@@ -5,7 +5,6 @@ helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
 kubectl config set-context --current --namespace=cert-manager
-
 helm template \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
