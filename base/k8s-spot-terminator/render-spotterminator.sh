cb0c6a3fceb58922271c0c3b175013600b3f6d53
timothy-spencer <timothy.spencer@gsa.gov>
2020-09-04 10:07:27 -0700

    try different spot terminator

--- base/k8s-spot-terminator/render-spotterminator.sh
+++ base/k8s-spot-terminator/render-spotterminator.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+#
+# This used to be done with https://github.com/pusher/k8s-spot-termination-handler,
+# but this AWS supported one looks better:
+# https://github.com/aws/aws-node-termination-handler
+#
 
+helm repo add eks https://aws.github.io/eks-charts
 helm repo update
 
-helm template eks stable/k8s-spot-termination-handler --namespace kube-system > k8s-spot-termination-handler.yaml
+#helm template eks stable/k8s-spot-termination-handler --namespace kube-system > k8s-spot-termination-handler.yaml
+
+helm template aws-node-termination-handler eks/aws-node-termination-handler \
+  --namespace kube-system \
+  --set enableSpotInterruptionDraining="true" \
+  --set deleteLocalData="true" \
+  --set jsonLogging="true" \
+  --set enablePrometheusServer="true" > aws-node-termination-handler.yaml
 
