175d28f143892b086b55b16e1cb869231589bcff
timothy-spencer <timothy.spencer@gsa.gov>
2020-09-08 16:56:23 -0700

    remove pdns stuff

--- base/pdns/cdwv/render-pdns.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/cdwv/powerdns-helm
-
-helm template pdns powerdns-helm > powerdns.yaml
-
-rm -rf powerdns-helm
-
--- base/pdns/render-pdns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-helm repo add halkeye https://halkeye.github.io/helm-charts/
-helm repo update
-
-helm template pdns halkeye/powerdns --version 0.2.0 > powerdns.yaml
-
