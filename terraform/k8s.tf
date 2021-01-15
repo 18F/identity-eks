e116b6b1aa312ef4d93b573eee138afcdb293893
timothy-spencer <timothy.spencer@gsa.gov>
2021-01-15 10:30:38 -0800

    remove some idp cruft we do not need

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,13 +1,4 @@
 
-resource "kubernetes_namespace" "idp" {
-  metadata {
-    name = "idp"
-    labels = {
-      istio-injection = "enabled"
-    }
-  }
-}
-
 resource "kubernetes_namespace" "elk" {
   metadata {
     name = "elk"
@@ -40,79 +31,7 @@ resource "kubernetes_namespace" "elk" {
 #   }
 # }
 
-resource "kubernetes_config_map" "idp-config" {
-  metadata {
-    name = "idp-config"
-    namespace = "idp"
-    labels = {
-      name = "idp-config"
-    }
-  }
 
-  data = {
-    db_hostname = aws_db_instance.idp.address
-    db_port = aws_db_instance.idp.port
-    domain_name = var.idp_hostname
-    mailer_domain_name = var.idp_hostname
-  }
-}
-
-resource "kubernetes_service" "idp-redis" {
-  metadata {
-    name = "idp-redis"
-    namespace = "idp"
-    labels = {
-      name = "idp-redis"
-    }
-  }
-
-  spec {
-    type = "ExternalName"
-    external_name = aws_elasticache_replication_group.idp.primary_endpoint_address
-    port {
-      port = 6379
-      protocol = "TCP"
-      target_port = 6379
-    }
-  }
-}
-
-resource "kubernetes_ingress" "idp-ingress" {
-  depends_on = [
-    helm_release.alb-ingress-controller,
-  ]
-  metadata {
-    name = "idp-ingress"
-    namespace = "istio-system"
-    annotations = {
-      "kubernetes.io/ingress.class" = "alb"
-      "alb.ingress.kubernetes.io/scheme" = "internet-facing"
-      "alb.ingress.kubernetes.io/certificate-arn" = module.acm-cert-idp.cert_arn
-      "alb.ingress.kubernetes.io/backend-protocol" = "HTTPS"
-      "alb.ingress.kubernetes.io/healthcheck-protocol" = "HTTPS"
-      # XXX These are here for the future when we enable WAF
-      # alb.ingress.kubernetes.io/waf-acl-id" = "XXX"
-      # alb.ingress.kubernetes.io/wafv2-acl-arn" = "XXX"
-      "alb.ingress.kubernetes.io/load-balancer-attributes" = "access_logs.s3.enabled=true,access_logs.s3.bucket=login-gov.elb-logs.${data.aws_caller_identity.current.account_id}-${var.region},access_logs.s3.prefix=${var.cluster_name}/idp"
-      # limit access
-      "alb.ingress.kubernetes.io/inbound-cidrs" = join(", ", var.kubecontrolnets)
-    }
-  }
-
-  spec {
-    rule {
-      http {
-        path {
-          path = "/*"
-          backend {
-            service_name = "istio-ingressgateway"
-            service_port = 443
-          }
-        }
-      }
-    }
-  }
-}
 
 # # XXX this is not going to work because istio isn't going yet.  :-(
 # resource "kubernetes_manifest" "idp_gateway" {
